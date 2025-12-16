613c534a31683843fa1a12f8e587e6eefe932318
yeahieaKhan <yeahieakhan2017@gmail.com>
2025-12-16 21:51:16 +0600

    learing distrucing

--- src/distructring.ts
+++ src/distructring.ts
@@ -1,14 +1,39 @@
-const user = {
+// const user = {
+//     id: 123,
+//     name: {
+//         firstName: "Sohag",
+//         middleName: "hossain",
+//         lastName:"Jewel"
+//     }
+// }
+
+
+// const { name: { firstName, middleName, lastName },} = user;
+
+
+// console.log(firstName)
+
+
+type User = {
+    id: number,
+    name: {
+        firstName: string,
+        middleName?: string,
+        lastName:string,
+    }
+    isMarried:boolean
+}
+
+const user: User = {
     id: 123,
     name: {
         firstName: "Sohag",
-        middleName: "hossain",
-        lastName:"Jewel"
-    }
+        middleName: "Hossain",
+        lastName: "Raj"
+    },
+    isMarried:true
 }
 
 
-const { name: { firstName, middleName, lastName },} = user;
-
-
+const { name: { firstName, middleName, lastName } } = user;
 console.log(firstName)
