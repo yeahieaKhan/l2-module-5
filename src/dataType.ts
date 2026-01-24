fa0ff881a8efdfe359592f42cefbe26bf5378263
yeahieaKhan <yeahieakhan2017@gmail.com>
2026-01-24 22:01:51 +0600

    type alias with object

--- src/dataType.ts
+++ src/dataType.ts
@@ -31,8 +31,61 @@ let name: string = "Sohag";
 
 console.log(name.charAt);
 
-let person = {
-    name: "Sohag",
-    age: 25,
-    city: "Sirajgaj",
+
+
+// Type declarations
+
+
+// let person: {
+//     name: string,
+//     age: number,
+//     city:string
+// } =  {
+//     name: "Sohag",
+//     age: 25,
+//     city: "Sirajgaj",
+// }
+
+// console.log(person)
+
+
+
+
+// type alias
+
+type TPerson = {
+    name: string,
+    age: number,
+    city: string,
+    address: {
+        village: string,
+        wordNumber: number,
+    }
 }
+
+
+
+
+let person: TPerson = {
+    name: "Sohag",
+    age: 24,
+    city:"Sirajganj",
+    address: {
+        village: "Charpodom pal",
+        wordNumber: 15
+    }
+}
+
+console.log(person)
+
+let person2: TPerson = { 
+    name: "asif",
+    age: 24,
+    city: "belkuchi",
+    address: {
+        village: "belkuchi",
+        wordNumber: 15
+    }
+}
+
+console.log(person2)
