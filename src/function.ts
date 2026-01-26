7952a6d28c7a873c212b2f0722a1c53ede2f7da7
yeahieakhan <yeahieakhan2017@gmail.com>
2026-01-26 11:32:59 -0600

    done function

--- src/function.ts
+++ src/function.ts
@@ -4,3 +4,11 @@ const add = (a: number, b: number): number => {
 
 const result = add(5, 10);
 console.log(`The result is: ${result}`);
+
+
+const subtract = (a: number, b: number): number => {
+    return a - b;
+}
+
+const result2 = subtract(10, 5);
+console.log(`The result is: ${result2}`);
