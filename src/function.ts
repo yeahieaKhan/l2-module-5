6f1d6bfc0a2454aac226ef33a8389696d6e0c942
yeahieakhan <yeahieakhan2017@gmail.com>
2026-01-26 11:35:48 -0600

    function done

--- src/function.ts
+++ src/function.ts
@@ -12,3 +12,14 @@ const subtract = (a: number, b: number): number => {
 
 const result2 = subtract(10, 5);
 console.log(`The result is: ${result2}`);
+
+
+const login =(email:string,password:string):string=>{
+    if(email === "user@gmail.com" && password === "password123"){
+        return "Login successful";
+    } else {
+        return "Login failed";
+    }
+}
+
+console.log(login("user@gmail.com", "password123"));
